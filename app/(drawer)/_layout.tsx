75e0fdaf267cc2a10b95dff1b2726d64d2197482
rGupta711 <116116756+rGupta711@users.noreply.github.com>
2023-11-26 17:58:04 -0500

    custom drawer navigation

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,10 +1,39 @@
-import { Stack, withLayoutContext } from "expo-router";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { withLayoutContext } from 'expo-router';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
+import { ActivityIndicator, Text } from 'react-native';
 
 const DrawerNavigator = createDrawerNavigator().Navigator;
 
-const Drawer = withLayoutContext(DrawerNavigator)
+const Drawer = withLayoutContext(DrawerNavigator);
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <Text style={{ alignSelf: 'center', fontSize: 20 }}>Vadim</Text>
+
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
 
 export default function DrawerLayout() {
-    return <Drawer />
+  return (
+    <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen
+        name="(tabs)"
+        options={{ headerShown: false, title: 'My Feed' }}
+      />
+      <Drawer.Screen name="bookmarks" options={{ title: 'Bookmarks' }} />
+      <Drawer.Screen name="twitter-blue" options={{ title: 'Twitter Blue' }} />
+    </Drawer>
+  );
 }
